66eb580e0304af45832cb5ee69ae13a0609d5556
”Saksham <iamsakshamrathore@gmail.com>
2026-06-22 02:32:05 +0530

    Palindrome Number

---
+++ 9_Palindrome_Number.cpp
@@ -0,0 +1,25 @@
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool palindrome = false;
+        long long rev = 0;
+        long long digits = 0;
+        long long orignal = x;
+        if(x < 0)
+        return false;
+        while( x > 0){
+            digits = x%10;
+            rev = rev*10 + digits;
+            x/=10;
+        }
+        if(rev==orignal){
+            palindrome = true;
+        }
+        
+        return palindrome;
+    }
+};
