af4290f378a5cf50d8e2df1ab22f146bc7bd5bc0
Saksham Rathore <iamsakshamrathore@gmail.com>
2026-09-16 21:03:49 +0530

    Time: 8 ms (46.24%), Space: 47.5 MB (17.74%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -11,20 +11,39 @@
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        
-        vector<int>res;
+        // **Brute Force Approach :-**
+//         vector<int>res;
+//         while(head){
+//             ListNode* temp=head->next;
+//             while(temp && temp->val<=head->val){
+//                 temp=temp->next;
+//             }
+//             if(temp){
+//                 res.push_back(temp->val);
+//             }else{
+//                 res.push_back(0);
+//             }
+//             head=head->next;
+//         }
+//         return res;
+
+vector<int> arr;
         while(head){
-            ListNode* temp=head->next;
-            while(temp && temp->val<=head->val){
-                temp=temp->next;
-            }
-            if(temp){
-                res.push_back(temp->val);
-            }else{
-                res.push_back(0);
-            }
+            arr.push_back(head->val);
             head=head->next;
         }
+        stack<int> st;
+
+        vector<int> res(arr.size(),0);
+
+        for(int i=0;i<arr.size();i++){
+            while(!st.empty() && arr[st.top()]<arr[i]){
+                res[st.top()]=arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
         return res;
-    }
-};
+   }
+ };
+
